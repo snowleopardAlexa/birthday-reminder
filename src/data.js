ee491b5bd1743db4c9890825c5b65f0365819049
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-24 10:10:39 -0500

    add data.js

---
+++ src/data.js
@@ -0,0 +1,41 @@
+import freddie from './img/freddie.jpeg';
+import elton from './img/elton.jpeg';
+import tina from './img/tina.jpeg';
+import celine from './img/celine.jpeg';
+import david from './img/david.jpeg';
+
+const birthday = [
+    {
+        id: 1,
+        name: 'Freddie Mercury',
+        age: 45,
+        img: freddie
+    },
+    {
+        id: 2,
+        name: 'Elton John',
+        age: 72,
+        img: elton
+    },
+    {
+        id: 3,
+        name: 'Tina Turner',
+        age: 75,
+        img: tina
+    },
+    {
+        id: 4,
+        name: 'Celine Dion',
+        age: 52,
+        img: celine
+    },
+    {
+        id: 5,
+        name: 'David Bowie',
+        age: 65,
+        img: david
+    },
+
+]
+
+export default birthday;
