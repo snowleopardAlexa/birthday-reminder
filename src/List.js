cb78aa709c307684db6efc560bff5cdf73f7b8c4
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-24 10:30:09 -0500

    finish app

--- src/List.js
+++ src/List.js
@@ -2,10 +2,10 @@
   return (
     <>
       {people.map((person) => {
-          const {id, name, age, image} = person;
+          const {id, name, age, img} = person;
           return (
             <article key={id} className="person">
-                <img src={image} alt={name} />
+                <img src={img} alt={name} />
                 <div>
                     <h4>{name}</h4>
                     <p>{age} years</p>
